73c7473aed775ce5ccec232ede11bfd342266ac3
akashsrinivsan15 <akashsrinivasan151@gmial.com>
2025-08-10 23:24:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const navLinks = document.getElementById("navLinks");
+
+navLinks.style.maxHeight = "0px";
+
+function menuToggle() {
+    if (navLinks.style.maxHeight == "0px") {
+        navLinks.style.maxHeight = "200px";
+    } else {
+        navLinks.style.maxHeight = "0px";
+    }
+}
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    }
+    else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    }
+    else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+setInterval(() => nextSlide(), 5000);
